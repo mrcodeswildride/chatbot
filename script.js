56ce299e7f14be4db7a25bb2570c084eacab2778
David Keller <kellerdb@gmail.com>
2021-05-06 16:52:49 -0700

    formatting

--- script.js
+++ script.js
@@ -12,50 +12,38 @@ message.addEventListener(`keydown`, keyPressed)
 message.focus()
 
 function send() {
-  let messageValue = message.value.trim()
+  let messageValue = message.value.trim().toLowerCase()
+  you.innerHTML = `<strong>You:</strong> ${messageValue}`
 
-  if (messageValue != ``) {
-    you.innerHTML = `<strong>You:</strong> ${messageValue}`
-
-    if (jokeMode) {
-      if (messageValue.toLowerCase() == `two kilo mockingbird`) {
-        bot.innerHTML = `<strong>Bot:</strong> you must have heard this joke before`
-      }
-      else {
-        bot.innerHTML = `<strong>Bot:</strong> two kilo mockingbird`
-      }
-
-      jokeMode = false
+  if (jokeMode) {
+    if (messageValue == `two kilo mockingbird`) {
+      bot.innerHTML = `<strong>Bot:</strong> you must have heard this joke before`
+    } else {
+      bot.innerHTML = `<strong>Bot:</strong> two kilo mockingbird`
     }
-    else if (squareRootMode) {
+
+    jokeMode = false
+  } else if (squareRootMode) {
+    if (messageValue == ``) {
+      bot.innerHTML = `<strong>Bot:</strong> type a number`
+    } else {
       let squareRoot = Math.sqrt(messageValue)
-
-      if (isNaN(squareRoot)) {
-        bot.innerHTML = `<strong>Bot:</strong> please type a number`
-      }
-      else {
-        bot.innerHTML = `<strong>Bot:</strong> the square root of ${messageValue} is ${squareRoot}`
-        squareRootMode = false
-      }
+      bot.innerHTML = `<strong>Bot:</strong> the square root of ${messageValue} is ${squareRoot}`
+      squareRootMode = false
     }
-    else {
-      if (messageValue.toLowerCase() == `how are you`) {
-        bot.innerHTML = `<strong>Bot:</strong> I'm doing great`
-      }
-      else if (messageValue.toLowerCase() == `tell a joke`) {
-        bot.innerHTML = `<strong>Bot:</strong> what do you call 2000 mockingbirds?`
-        jokeMode = true
-      }
-      else if (messageValue.toLowerCase() == `do a square root`) {
-        bot.innerHTML = `<strong>Bot:</strong> please type a number`
-        squareRootMode = true
-      }
-      else if (messageValue.toLowerCase() == `xyzzy`) {
-        bot.innerHTML = `<strong>Bot:</strong> nothing happens`
-      }
-      else {
-        bot.innerHTML = `<strong>Bot:</strong> I don't understand`
-      }
+  } else {
+    if (messageValue == `how are you`) {
+      bot.innerHTML = `<strong>Bot:</strong> I'm doing great`
+    } else if (messageValue == `tell a joke`) {
+      bot.innerHTML = `<strong>Bot:</strong> what do you call 2000 mockingbirds?`
+      jokeMode = true
+    } else if (messageValue == `do a square root`) {
+      bot.innerHTML = `<strong>Bot:</strong> type a number`
+      squareRootMode = true
+    } else if (messageValue == `xyzzy`) {
+      bot.innerHTML = `<strong>Bot:</strong> nothing happens`
+    } else {
+      bot.innerHTML = `<strong>Bot:</strong> I don't understand`
     }
   }
 
